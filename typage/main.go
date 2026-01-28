b1470a067b83e5e09fa30581e5d82372e6da73ac
julestristan <tristanjules7@gmail.com>
2026-01-29 00:05:17 +0100

    test sprintf & typing

--- typage/main.go
+++ typage/main.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	WriteMessage("Test")
+	WriteMessage("Test\n")
+	fmt.Printf(AppendMessage("Hello", "World"))
 }
+
 func WriteMessage(message string) {
 	fmt.Printf("voici le message: %v", message)
+}
+
+func AppendMessage(m1 string, m2 string) string {
+	return fmt.Sprintf("Message 1: %v\nMessage 2: %v", m1, m2)
 }
